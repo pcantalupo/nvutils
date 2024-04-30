78ecf4205677347276c6835fd34e3b9bef2e9bcd
Paul Cantalupo <pcantalupo@gmail.com>
2024-04-30 13:49:23 -0400

    adding params (e.g. guess_max) to tsv2xlsx() and tsv2xlsx.R

--- R/tsv2xlsx.R
+++ R/tsv2xlsx.R
@@ -2,6 +2,9 @@
 #' @description Read TSV file and output it as an XLSX file
 #' @param file A tab-separated (.tsv) file (with header row)
 #' @param outfile Excel file name
+#' @param show_col_types See ?readr::read_tsv
+#' @param guess_max See ?readr::read_tsv. Set to 'Inf' to use all rows to
+#' determine column type.
 #'
 #' @return Nothing
 #' @export
@@ -12,7 +15,7 @@
 #' \dontrun{
 #' tsv2xlsx("foo.tsv", "foo.xlsx")
 #' }
-tsv2xlsx = function(file, outfile) {
+tsv2xlsx = function(file, outfile, show_col_types = NULL, guess_max = 1000) {
   if(missing(file) || missing(outfile)) {
     stop("Usage: tsv2xlsx(file, outfile)")
   }
@@ -21,7 +24,7 @@ tsv2xlsx = function(file, outfile) {
     stop(file, " not found")
   }
 
-  tsv = read_tsv(file, show_col_types = FALSE)
+  tsv = read_tsv(file, show_col_types = show_col_types, guess_max = guess_max)
   write.xlsx(tsv, outfile)
 }
 
--- inst/scripts/tsv2xlsx.R
+++ inst/scripts/tsv2xlsx.R
@@ -4,8 +4,17 @@ pacman::p_load('nvutils')
 args = commandArgs(trailingOnly = TRUE)
 file = args[1]
 outfile = args[2]
+guess_max = args[3]
+show_col_types = args[4]
 
-tsv2xlsx(file, outfile)
+if (is.na(guess_max)) {
+  guess_max = 1000        # same default as readr:read_tsv
+}
+if (is.na(show_col_types)) {
+  show_col_types = NULL   # same default as readr::read_tsv
+}
+
+tsv2xlsx(file, outfile, guess_max = guess_max, show_col_types = show_col_types)
 
 
 cat("\n\n")
