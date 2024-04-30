2198b9aa3dc73f9fcbc5b29dd9ee99e2ffba2621
Paul Cantalupo <pcantalupo@gmail.com>
2024-04-30 14:50:41 -0400

    fixing guess_max parameter in tsv2xlsx.R

--- inst/scripts/tsv2xlsx.R
+++ inst/scripts/tsv2xlsx.R
@@ -9,10 +9,14 @@ show_col_types = args[4]
 
 if (is.na(guess_max)) {
   guess_max = 1000        # same default as readr:read_tsv
-}
+} else if (guess_max == "Inf") {
+  guess_max = Inf
+} else { guess_max = as.integer(guess_max) }
+
 if (is.na(show_col_types)) {
   show_col_types = NULL   # same default as readr::read_tsv
 }
+
 
 tsv2xlsx(file, outfile, guess_max = guess_max, show_col_types = show_col_types)
 
